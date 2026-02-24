e3a643ff007712cf3971c1b8191418754c91e0e4
Dent775 <dentsec151@gmail.com>
2026-02-24 19:40:32 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (53.22%) - LeetSync

---
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int dfs(TreeNode*root,int num)
+    {
+        if(root==nullptr)
+        {
+            return 0;
+        }
+        num=num*2+root->val;
+        if(root->right==nullptr&&root->left==nullptr)
+        {
+            return num;
+        }
+        return dfs(root->left,num)+dfs(root->right,num);;
+    }
+
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root,0);
+    }
+};
