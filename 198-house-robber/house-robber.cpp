2c957c1b1455219b1d64962fbb0cb8132255b00f
Dent775 <dentsec151@gmail.com>
2026-03-02 16:38:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (61.83%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    int solve(int n, vector<int>& nums,vector<int>& dp)
+    {
+        if(n==0)
+        return nums[0];
+        if(n<0)
+        return 0;
+        if(dp[n]!=-1)
+        return dp[n];
+        int pick=nums[n]+solve(n-2,nums,dp);
+        int npick=0+solve(n-1,nums,dp);
+        return dp[n]=max(pick,npick);
+    }
+    
+    int rob(vector<int>& nums) {
+      
+      int n=nums.size();
+      vector<int>dp(n,-1);
+      
+      return solve(n-1,nums,dp);  
+    }
+};
