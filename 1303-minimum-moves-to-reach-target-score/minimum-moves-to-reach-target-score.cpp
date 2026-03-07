4d8b1e56fc485fc915fb75f2992439ede5a6b04c
Dent775 <dentsec151@gmail.com>
2026-03-07 13:31:04 +0530

    Time: 1360 ms (8.72%) | Memory: 8 MB (30.60%) - LeetSync

---
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minMoves(int target, int maxDoubles) {
+        int count=0;
+        if(maxDoubles==0)
+        return target-1;
+        while(target>1)
+        {
+            if(target&1)
+            {
+                target--;
+                count++;
+            }
+            else
+            {
+                if(maxDoubles>0)
+                {
+                    target>>=1;
+                    count++;
+                    maxDoubles--;
+                }
+                else
+                {
+                    target--;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
